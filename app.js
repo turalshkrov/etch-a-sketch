c790354fe1f52390c8d4cda848ef410307ec1c5b
Tural Şükürov <tural.shkrov@gmail.com>
2023-03-08 00:48:06 +0400

    feat: Resize grid

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const colorInput = document.getElementById('color-input');
 const sizeInput = document.getElementById('size-input');
 const modeButtons = [...document.getElementById('buttons').children];
 const clearBtn = document.getElementById('clear');
+const sizeLabel = document.getElementById('grid-size');
 
 const INITIAL_SIZE = 16;
 const INITIAL_COLOR = '#202020';
@@ -37,6 +38,8 @@ const paint = (e) => {
 }
 
 const gridSetup = () => {
+    grid.style = `grid-template-columns: repeat(${currentSize}, 1fr); grid-template-rows: repeat(${currentSize}, 1fr)`;
+    grid.innerHTML = '';
     for (let i = 0; i < currentSize * currentSize; i++) {
         const gridElement = document.createElement('div');
         gridElement.classList.add('grid-element');
@@ -60,12 +63,18 @@ const clear = () => {
     [...grid.children].forEach(gridElement => gridElement.style = ``);
 }
 
+const reSize = () => {
+    currentSize = sizeInput.value;
+    gridSetup();
+    sizeLabel.innerText = `${currentSize} x ${currentSize}`;
+}
+
 const addEventListeners = () => {
     modeButtons.forEach(button => button.addEventListener('click', modeUpdate));
     colorInput.addEventListener('change', colorUpdate);
     clearBtn.addEventListener('click', clear);
+    sizeInput.addEventListener('change', reSize);
 }
-
 
 gridSetup();
 addEventListeners();
