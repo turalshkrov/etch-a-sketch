1b7524f28473f0fa62e698d07d559083065b2e28
Tural Şükürov <tural.shkrov@gmail.com>
2023-03-07 22:50:13 +0400

    feat: Paint and clear

--- app.js
+++ app.js
@@ -1,11 +1,71 @@
 const grid = document.getElementById('grid');
+const colorInput = document.getElementById('color-input');
+const sizeInput = document.getElementById('size-input');
+const modeButtons = [...document.getElementById('buttons').children];
+const clearBtn = document.getElementById('clear');
 
-let sizeOfGrid = 16;
-let i = 0;
+const INITIAL_SIZE = 16;
+const INITIAL_COLOR = '#202020';
 
-for (let i = 0; i < sizeOfGrid * sizeOfGrid; i++) {
-    const gridElement = document.createElement('div');
-    gridElement.classList.add('grid-element');
-    grid.appendChild(gridElement);
+let currentSize = INITIAL_SIZE;
+let currentColor = INITIAL_COLOR;
 
+let mode = 'color';
+
+let mouseDown = false;
+document.addEventListener('mousedown', () => mouseDown = true);
+document.addEventListener('mouseup', () => mouseDown = false);
+
+const paint = (e) => {
+    if ((e.type === 'mouseover' && mouseDown) || e.type === 'click') {
+        switch (mode) {
+            case 'color':
+                e.target.style = `background: ${currentColor}`;
+                break;
+            case 'rainbow':
+                const randomR = Math.floor(Math.random() * 256);
+                const randomG = Math.floor(Math.random() * 256);
+                const randomB = Math.floor(Math.random() * 256);
+                e.target.style = `background: rgb(${randomR}, ${randomG}, ${randomB})`;
+                break;
+            case 'eraser':
+                e.target.style = `background: #fff`;
+            default:
+                break;
+        }
+    }
 }
+
+const gridSetup = () => {
+    for (let i = 0; i < currentSize * currentSize; i++) {
+        const gridElement = document.createElement('div');
+        gridElement.classList.add('grid-element');
+        gridElement.addEventListener('click', paint);
+        gridElement.addEventListener('mouseover', paint);
+        grid.appendChild(gridElement);
+    }
+}
+
+const modeUpdate = (e) => {
+    mode = e.target.id;
+    modeButtons.map(button => button.classList.remove('button-active'));
+    modeButtons.filter(button => button.id === mode)[0].classList.add('button-active');
+}
+
+const colorUpdate = () => {
+    currentColor = colorInput.value;
+}
+
+const clear = () => {
+    [...grid.children].forEach(gridElement => gridElement.style = ``);
+}
+
+const addEventListeners = () => {
+    modeButtons.forEach(button => button.addEventListener('click', modeUpdate));
+    colorInput.addEventListener('change', colorUpdate);
+    clearBtn.addEventListener('click', clear);
+}
+
+
+gridSetup();
+addEventListeners();
